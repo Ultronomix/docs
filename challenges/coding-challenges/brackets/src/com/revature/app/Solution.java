8d21d7ad43bcee29c6e71ac49d7590d24b7f95e7
Marco Younan <youmarconan@gmail.com>
2022-09-09 11:58:18 -0700

    solved

--- challenges/coding-challenges/brackets/src/com/revature/app/Solution.java
+++ challenges/coding-challenges/brackets/src/com/revature/app/Solution.java
@@ -1,8 +1,25 @@
 package com.revature.app;
 
+import java.util.ArrayList;
+
 public class Solution {
     public String brackets(String s) {
         /* Write your code here */
-        return "";
+        String x="";
+        ArrayList<Character> y = new ArrayList<>();
+        for(int i=0 ; i<s.length() ; i++){
+            y.add(s.charAt(i));
+        }
+        for(int i = 0 ; i< y.size() ; i++){
+            if(y.get(i).equals('.')){
+                x=x.concat("["+Character.toString(y.get(i))+"]");
+                i++;
+            }
+            boolean z = i>=y.size();
+            if(!z){
+            x=x.concat(Character.toString(y.get(i)));
+            }
+        }
+        return x;
     }
 }
