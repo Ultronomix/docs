d22dcf6508944d7a9b342f1a0d1cf28a907e9608
NickNSX <nickogir@gmail.com>
2022-09-09 18:40:18 -0500

    Sum of Array

---
+++ challenges/coding-challenges/sum-of-array/SumOrArray.java
@@ -0,0 +1,23 @@
+//* Sum of array */
+
+public class SumOrArray {
+    public static void main(String[] args) {
+        
+        int[] numbers = {1, 2, 3, 4, 5};
+
+        int total = 0;
+
+        for (int i : numbers) {
+            total += numbers[i-1];
+        }
+
+        System.out.print("Numbers in array: ");
+
+        for (int i : numbers) {
+            System.out.print(numbers[i-1] + " ");
+        }
+
+        System.out.println("\nTotal is: " + total);
+    }
+    
+}
