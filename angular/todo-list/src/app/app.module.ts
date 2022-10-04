426263b2057b1e9d89cce28d7c95e0e318a54942
benp3837 <petruzziellbj@alumni.vcu.edu>
2022-10-04 11:20:24 -0400

    anguar todo list

--- angular/todo-list/src/app/app.module.ts
+++ angular/todo-list/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -6,17 +7,20 @@ import { AppComponent } from './app.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { TodosComponent } from './components/todos/todos.component';
 
+//Without listing our components, modules, and services in @NgModule...
+//Angular wouldn't recognize any of the things we're using
 @NgModule({
-  declarations: [
+  declarations: [ //declarations are where we list our components
     AppComponent,
     BannerComponent,
     TodosComponent
   ],
-  imports: [
+  imports: [ //imports are where we import new modules (like java packages)
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
-  providers: [],
+  providers: [], //providers are where we list our services
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- angular/todo-list/src/app/components/todos/todos.component.ts
+++ angular/todo-list/src/app/components/todos/todos.component.ts
@@ -7,6 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodosComponent implements OnInit {
 
+  //a variable that determines whether our greeterDiv renders
+  greeterDivToggle:boolean = true;
+  //a variable that holds the user inputted name
+  name:string = "";
+  //this variable gets updated once the user submits their name
+  finalName:string = "";
+
+
   //a variable that determines whether the todo cards are hidden or not
   hiddenToggle:boolean = true;
   //a variable that determines what the button says
@@ -25,6 +33,33 @@ export class TodosComponent implements OnInit {
     status: "Incomplete"
   }
 
+  todo3 = {
+    id: 3,
+    task: "put a little makeup",
+    status: "Incomplete"
+  }
+
+  todo4 = {
+    id: 4,
+    task: "have a great day",
+    status: "Incomplete"
+  }
+
+  todo5 = {
+    id: 5,
+    task: "go to sleep",
+    status: "Incomplete"
+  }
+
+  todo6 = {
+    id: 6,
+    task: "go to sleep again",
+    status: "Incomplete"
+  }
+
+  //make an Array to hold my todo objects in order to use in the *ngFor directive
+  todosArray = [this.todo1, this.todo2, this.todo3, this.todo4, this.todo5, this.todo6]
+
   constructor() { }
 
   ngOnInit(): void {
@@ -41,5 +76,12 @@ export class TodosComponent implements OnInit {
       this.buttonMessage = "Hide Tasks"
     }
   } 
+
+  //function to toggle the greeter div funcitonality after the user submits their name
+  //it will also set the "finalName" variable to whatever is in the "name" variable
+  submitName(){
+    this.finalName = this.name;
+    this.greeterDivToggle = false;
+  }
 
 }
